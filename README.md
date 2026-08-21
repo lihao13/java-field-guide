7181604a1a30b4e3ff2e40548d1780d3fe58d8d5
jianglihao <jianglihao@myhexin.com>
2026-08-21 13:49:45 +0800

    Add migrated Java Field Guide app

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Java Field Guide
 
-一个面向前端开发者的 Java 后端实战学习路线静态站点。
+一个面向前端开发者的 Java 后端实战学习路线站点，迁移自 [java.evanqhu.me](https://java.evanqhu.me)。
 
 ## 本地预览
 
-直接打开 `index.html` 即可，或运行：
+这是一个 React SPA 的静态构建产物，包含首页、六个 `/learn/:slug` 学习章节和 `/glossary` 术语页。
+
+本地预览：
 
 ```bash
 python3 -m http.server 4173
@@ -12,6 +14,10 @@ python3 -m http.server 4173
 
 然后访问 http://localhost:4173。
 
-## 免费部署
+## 部署
 
-这是纯静态网站，可以直接部署到 GitHub Pages、Cloudflare Pages 或 Vercel。无需数据库，学习进度保存在浏览器 localStorage 中。
+项目已配置为 GitHub Pages 项目站点，地址为：
+
+https://lihao13.github.io/java-field-guide/
+
+学习进度和主题设置保存在浏览器 `localStorage` 中，无需数据库。
---
+++ assets/clock-3-C51Bx5GL.js
@@ -0,0 +1,16 @@
+import{c}from"./index-LKLREVwp.js";/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]],a=c("arrow-right",o);/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]],i=c("circle-check",e);/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["path",{d:"M12 6v6h4",key:"135r8i"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]],h=c("clock-3",r);export{a as A,i as C,h as a};
